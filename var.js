5425a58929539b403c70ad50c88f1fc243b4783a
khamidn <nurkhamid014@gmail.com>
2020-11-26 05:14:16 +0700

    variable

---
+++ var.js
@@ -0,0 +1,11 @@
+let camelCase = 12;
+const fixValue = 10;
+
+console.log(camelCase, fixValue);
+
+camelCase = 5;
+//jika rubah nilai di sini 
+// fixValue = 6;
+//dan tampilkan di sini maka
+// console.log(camelCase, fixValue);
+console.log(camelCase);
